6839e51a3dc82760b7ab33eda3d381348f9c871f
partha-nm <71543694+partha-nm@users.noreply.github.com>
2022-01-04 11:39:14 +0530

    Time: 7 ms (47.51%), Space: 11.6 MB (82.24%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) 
+    vector<int> targetIndices(vector<int>& v, int target) 
     {
         vector<int>ans;
-        sort(nums.begin(),nums.end());
-        int lo = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        if(lo == nums.size() || nums[lo] != target) return {};
-        int hi = upper_bound(nums.begin(), nums.end(), target) - nums.begin()-1;
-        if(lo == hi) return {lo};
-        for(int i=lo; i<=hi; i++) ans.push_back(i);
+        sort(v.begin(),v.end());
+        for(int i=0; i<v.size(); i++) if(v[i] == target) ans.push_back(i);
+        if(ans.size() == 0) return {};
         return ans;
     }
 };

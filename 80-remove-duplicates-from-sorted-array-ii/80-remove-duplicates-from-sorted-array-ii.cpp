10c7a2fbb9575d2accb7971f22a626662d3e990d
partha-nm <71543694+partha-nm@users.noreply.github.com>
2022-02-06 21:12:32 +0530

    Time: 7 ms (60.40%), Space: 10.8 MB (93.81%) - LeetHub

---
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+      int i =0;
+        for(int &x:nums)
+        {
+            if(i < 2 || x > nums[i-2]) {nums[i] = x; i++;}
+        }
+        return i;
+    }
+};

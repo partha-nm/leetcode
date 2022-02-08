30c6195d04e93fcdab3e984774497b88f8ba2cff
partha-nm <71543694+partha-nm@users.noreply.github.com>
2022-02-08 09:36:10 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    bool countDigit(int n)
-    {
-        int p = log10(n)+1;
-        if(p == 1) return true;
-        return false;
-    }
+    
     int addDigits(int num) 
     {
-        if(num == 0) return num;
-        while(!countDigit(num))
-        {
-            string s = to_string(num);
-            num =0;
-            for(int i=0; i<s.size(); i++)
-            {
-                num+= s[i]-'0';
-            }
-        }
-        return num;
+        if(num == 0) return 0;
+        else if(num%9 == 0) return 9;
+        else return num%9;
+        return 0;
     }
 };

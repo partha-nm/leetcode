fc13544a00514f7e8fb7a404ce3a21764002e197
partha-nm <71543694+partha-nm@users.noreply.github.com>
2022-06-09 00:13:23 +0530

    Time: 3 ms (39.03%), Space: 6.3 MB (26.79%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) 
+    {
+        string s1 = s;
+        reverse(s1.begin(), s1.end());
+        if(s1 == s) return 1;
+        return 2;
+    }
+};

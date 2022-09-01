7050079d395dd6322f0f66f7840174cda9a12b0a
partha-nm <71543694+partha-nm@users.noreply.github.com>
2022-09-02 01:47:21 +0530

    Time: 50 ms (67.28%), Space: 30.2 MB (7.26%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int solve(string &s, string &t, int i, int j, vector<vector<int>>&dp)
-    {
-        if(s.size() == i && t.size() == j) return 1; 
-        if(s.size() == i) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j])
-        {
-            return dp[i][j] = solve(s, t, i+1, j, dp) + solve(s, t, i+1, j+1, dp);
-        }
-        else
-        {
-            return dp[i][j] = solve(s, t , i+1, j, dp);
-        }
-    }
+    
     int numDistinct(string s, string t) 
     {
-        int i =0, j =0;
+         int i =0, j =0;
         int n = s.size(), m = t.size();
-        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        return solve(s, t, i, j, dp);
+        vector<vector<unsigned long long>>dp(n+1, vector<unsigned long long>(m+1, 0));
+        // dp[n][m] = 1;
+        for(int i=n; i>=0; i--)
+        {
+            for(int j=m; j>=0; j--)
+            {
+                if(i == n && j == m) dp[i][j] = 1;
+                else if(i == n) dp[i][j] = 0;
+                else
+                {
+                    if(s[i] == t[j]) dp[i][j] = dp[i+1][j] + dp[i+1][j+1];
+                    else dp[i][j] = dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };

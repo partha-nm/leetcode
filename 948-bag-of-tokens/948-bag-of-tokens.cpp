b954fcc2f65b32b27a91577f53db8f06e343c46e
partha-nm <71543694+partha-nm@users.noreply.github.com>
2022-09-13 02:39:50 +0530

    Time: 12 ms (42.99%), Space: 10.7 MB (27.64%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& t, int power) {
+        int ans =0;
+        sort(t.begin(), t.end());
+        int score = 0;
+        int i =0,j = t.size() -1;
+        while(i <= j)
+        {
+            if(t[i] <= power)
+            {
+                power-=t[i];
+                score++;
+                ans = max(ans, score);
+                i++;
+            }
+            else if(score > 0)
+            {
+                power+=t[j];
+                score--;
+                j--;
+            }
+            else break;
+        }
+        return ans;
+    }
+};
